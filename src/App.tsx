ec933964666cc1a88239863bb917cb1e143b11b8
Kadxy <2230318258@qq.com>
2025-03-18 13:42:49 +0800

    add initial configuration and layout for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
 import './App.css';
 
 const App = () => {
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
+            <div className='logo'>
+                <img src={'./logo.svg'} alt="logo"/>
             </div>
-            <h1>Kadxy</h1>
-            {/*<div className="card">*/}
-            {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
-            {/*    count is {count}*/}
-            {/*  </button>*/}
-            {/*  <p>*/}
-            {/*    Edit <code>src/App.tsx</code> and save to test HMR*/}
-            {/*  </p>*/}
-            {/*</div>*/}
-            <a className="read-the-docs" href={'https://beian.miit.gov.cn'} target="_blank">
-                粤ICP备2024164543号-3
-            </a>
+            <div className="title">
+                {import.meta.env.VITE_WEBSITE_TITLE || 'TITLE'}
+            </div>
+
+            {import.meta.env.VITE_WEBSITE_SUBTITLE &&
+                <div className="subtitle">
+                    {import.meta.env.VITE_WEBSITE_SUBTITLE}
+                </div>
+            }
+            {import.meta.env.VITE_WEBSITE_IPC_NAME &&
+                <a className="icp" href={'https://beian.miit.gov.cn'} target="_blank">
+                    {import.meta.env.VITE_WEBSITE_IPC_NAME}
+                </a>}
         </>
     );
 };
